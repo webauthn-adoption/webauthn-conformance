fe8982bfec176e29a6f7109171ce4e3d15ad2d60
Matthew Miller <matthew@millerti.me>
2020-08-15 15:28:21 -0700

    Initial Commit

---
+++ src/logger.ts
@@ -0,0 +1,17 @@
+import { log } from "./deps.ts";
+
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    webauthn: {
+      level: "DEBUG",
+      handlers: ["console"],
+    },
+  },
+});
+
+const logger = log.getLogger("webauthn");
+
+export default logger;
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import logger from "./logger.ts";
+
+logger.debug("hello");
