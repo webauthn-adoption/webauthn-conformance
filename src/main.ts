f0cced4905fef78af16841a1f1563f2c619079d5
Matthew Miller <matthew@millerti.me>
2020-08-29 22:57:39 -0700

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,39 @@
-import logger from "./helpers/logger.ts";
-import startTests from "./startTests.ts";
-import logTestResult from "./helpers/logTestResult.ts";
+import logger from "./helpers/logger";
+import startTests from "./startTests";
+import logTestResult from "./helpers/logTestResult";
 
-/**
- * Commandline arguments
- *
- * @param {string} args[0] Relying Party URL to test
- */
-const { args } = Deno;
+async function main() {
+  /**
+   * Commandline arguments
+   *
+   * @param {string} args[0] Relying Party URL to test
+   */
+  const { argv } = process;
 
-if (!args[0]) {
-  throw new Error("Relying Party URL is required");
+  if (!argv[0]) {
+    throw new Error("Relying Party URL is required");
+  }
+
+  const rpURL = argv[0];
+
+  const results = await startTests(rpURL);
+
+  // Mash all results together and then sort by test ID
+  const allResults = results
+    .sort((a, b) => {
+      const aID = a.identifier.id;
+      const bID = b.identifier.id;
+
+      if (aID < bID) return -1;
+      if (aID > bID) return 1;
+      return 0;
+    });
+
+  logger.info("[RESULTS]");
+
+  allResults.forEach((test) => {
+    logTestResult(test);
+  });
 }
 
-const rpURL = args[0];
-
-const results = await startTests(rpURL);
-
-// Mash all results together and then sort by test ID
-const allResults = results
-  .sort((a, b) => {
-    const aID = a.identifier.id;
-    const bID = b.identifier.id;
-
-    if (aID < bID) return -1;
-    if (aID > bID) return 1;
-    return 0;
-  });
-
-logger.info("[RESULTS]");
-
-allResults.forEach((test) => {
-  logTestResult(test);
-});
+main();
